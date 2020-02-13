dba946cb03928704c304ba2ab9f567d720afbcf6
Martin Ryan <martynmryan@gmail.com>
2020-02-13 15:10:13 +1100

    wip: runners health table

--- src/app/components/table/table-datasource.ts
+++ src/app/components/table/table-datasource.ts
@@ -3,36 +3,161 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { map } from 'rxjs/operators';
 import { Observable, of as observableOf, merge } from 'rxjs';
+import { format, subDays } from 'date-fns';
 
 // TODO: Replace this with your own data model type
+// export interface TableItem {
+//   name: string;
+//   id: number;
+//   active: string;
+//   description: string;
+//   ip_address: string;
+//   is_shared: string;
+//   online: string;
+//   status: string;
+// }
+
 export interface TableItem {
   name: string;
-  id: number;
+  minus7: object;
+  minus6: object;
+  minus5: object;
+  minus4: object;
+  minus3: object;
+  minus2: object;
+  minus1: object;
+  now: object;
 }
 
-// TODO: replace this with real data from your application
-const EXAMPLE_DATA: TableItem[] = [
-  {id: 1, name: 'Hydrogen'},
-  {id: 2, name: 'Helium'},
-  {id: 3, name: 'Lithium'},
-  {id: 4, name: 'Beryllium'},
-  {id: 5, name: 'Boron'},
-  {id: 6, name: 'Carbon'},
-  {id: 7, name: 'Nitrogen'},
-  {id: 8, name: 'Oxygen'},
-  {id: 9, name: 'Fluorine'},
-  {id: 10, name: 'Neon'},
-  {id: 11, name: 'Sodium'},
-  {id: 12, name: 'Magnesium'},
-  {id: 13, name: 'Aluminum'},
-  {id: 14, name: 'Silicon'},
-  {id: 15, name: 'Phosphorus'},
-  {id: 16, name: 'Sulfur'},
-  {id: 17, name: 'Chlorine'},
-  {id: 18, name: 'Argon'},
-  {id: 19, name: 'Potassium'},
-  {id: 20, name: 'Calcium'},
+const RUNNER_DATA: TableItem[] = [
+  {
+    name: 'one',
+    minus7: {
+      active: 'true',
+      description: 'runner-12a',
+      id: 1,
+      ip_address: '127.0.0.1',
+      is_shared: 'true',
+      name: 'seven',
+      online: 'true',
+      status: 'online'
+        },
+    minus6: {
+      active: 'true',
+      description: 'shared-runner-5b',
+      id: 6,
+      ip_address: '127.0.0.1',
+      is_shared: 'true',
+      name: 'three',
+      online: 'true',
+      status: 'online'
+        },
+    minus5: {
+      active: 'true',
+      description: 'test-2-78676965',
+      id: 85,
+      ip_address: '127.0.0.1',
+      is_shared: 'false',
+      name: 'five',
+      online: 'false',
+      status: 'offline'
+        },
+    minus4: {
+      active: 'false',
+      description: 'shared-runner-7',
+      id: 4,
+      ip_address: '127.0.0.1',
+      is_shared: 'true',
+      name: 'one',
+      online: 'true',
+      status: 'paused'
+    },
+    minus3: {
+      active: 'true',
+      description: 'shared-runner-1',
+      id: 3,
+      ip_address: '127.0.0.1',
+      is_shared: 'true',
+      name: 'three',
+      online: 'true',
+      status: 'online'
+        },
+    minus2: {
+      active: 'true',
+      description: 'shared-runner-2',
+      id: 2,
+      ip_address: '127.0.0.1',
+      is_shared: 'true',
+      name: 'two',
+      online: 'false',
+      status: 'offline'
+      },
+    minus1: {
+      active: 'true',
+      description: 'test-2-20150125',
+      id: 1,
+      ip_address: '127.0.0.1',
+      is_shared: 'false',
+      name: 'one',
+      online: 'false',
+      status: 'offline'
+        },
+    now: {
+      active: 'true',
+      description: 'shared-runner-1',
+      id: 0,
+      ip_address: '127.0.0.1',
+      is_shared: 'true',
+      name: 'one',
+      online: 'true',
+      status: 'online'
+    }
+  }
 ];
+
+// TODO: replace this with real data from gitlab
+// const RUNNER_DATA: TableItem[] = [
+//   {
+//       active: 'true',
+//       description: 'shared-runner-1',
+//       id: 1,
+//       ip_address: '127.0.0.1',
+//       is_shared: 'true',
+//       name: 'one',
+//       online: 'true',
+//       status: 'online'
+//   },
+//   {
+//       active: 'true',
+//       description: 'shared-runner-2',
+//       id: 3,
+//       ip_address: '127.0.0.1',
+//       is_shared: 'true',
+//       name: 'three',
+//       online: 'false',
+//       status: 'offline'
+//   },
+//   {
+//       active: 'true',
+//       description: 'test-1-20150125',
+//       id: 6,
+//       ip_address: '127.0.0.1',
+//       is_shared: 'false',
+//       name: 'six',
+//       online: 'true',
+//       status: 'paused'
+//   },
+//   {
+//       active: 'true',
+//       description: 'test-2-20150125',
+//       id: 8,
+//       ip_address: '127.0.0.1',
+//       is_shared: 'false',
+//       name: 'eight',
+//       online: 'false',
+//       status: 'offline'
+//   }
+// ];
 
 /**
  * Data source for the Table view. This class should
@@ -40,9 +165,17 @@ const EXAMPLE_DATA: TableItem[] = [
  * (including sorting, pagination, and filtering).
  */
 export class TableDataSource extends DataSource<TableItem> {
-  data: TableItem[] = EXAMPLE_DATA;
+  data: TableItem[] = RUNNER_DATA;
   paginator: MatPaginator;
   sort: MatSort;
+  nowminus7: any;
+  nowminus6: any;
+  nowminus5: any;
+  nowminus4: any;
+  nowminus3: any;
+  nowminus2: any;
+  nowminus1: any;
+  now: any;
 
   constructor() {
     super();
@@ -54,6 +187,7 @@ export class TableDataSource extends DataSource<TableItem> {
    * @returns A stream of the items to be rendered.
    */
   connect(): Observable<TableItem[]> {
+    this.getDates();
     // Combine everything that affects the rendered data into one update
     // stream for the data-table to consume.
     const dataMutations = [
@@ -62,9 +196,11 @@ export class TableDataSource extends DataSource<TableItem> {
       this.sort.sortChange
     ];
 
-    return merge(...dataMutations).pipe(map(() => {
-      return this.getPagedData(this.getSortedData([...this.data]));
-    }));
+    return merge(...dataMutations).pipe(
+      map(() => {
+        return this.getPagedData(this.getSortedData([...this.data]));
+      })
+    );
   }
 
   /**
@@ -94,11 +230,34 @@ export class TableDataSource extends DataSource<TableItem> {
     return data.sort((a, b) => {
       const isAsc = this.sort.direction === 'asc';
       switch (this.sort.active) {
-        case 'name': return compare(a.name, b.name, isAsc);
-        case 'id': return compare(+a.id, +b.id, isAsc);
-        default: return 0;
+        case 'name':
+          return compare(a.name, b.name, isAsc);
+        case 'id':
+          return compare(+a.id, +b.id, isAsc);
+        default:
+          return 0;
       }
     });
+  }
+
+  getDates() {
+    const n = new Date();
+    const nowminus7 = format(subDays(n, 7), 'ddMMM');
+    const nowminus6 = format(subDays(n, 6), 'ddMMM');
+    const nowminus5 = format(subDays(n, 5), 'ddMMM');
+    const nowminus4 = format(subDays(n, 4), 'ddMMM');
+    const nowminus3 = format(subDays(n, 3), 'ddMMM');
+    const nowminus2 = format(subDays(n, 2), 'ddMMM');
+    const nowminus1 = format(subDays(n, 1), 'ddMMM');
+    const now = format(n, 'ddMMM');
+    console.log('now: ', now);
+    console.log('nowminus1: ', nowminus1);
+    console.log('nowminus2: ', nowminus2);
+    console.log('nowminus3: ', nowminus3);
+    console.log('nowminus4: ', nowminus4);
+    console.log('nowminus5: ', nowminus5);
+    console.log('nowminus6: ', nowminus6);
+    console.log('nowminus7: ', nowminus7);
   }
 }
 
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -3,6 +3,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
 import { TableDataSource, TableItem } from './table-datasource';
+import { format, subDays } from 'date-fns';
 
 @Component({
   selector: 'app-table',
@@ -14,17 +15,46 @@ export class TableComponent implements AfterViewInit, OnInit {
   @ViewChild(MatSort, {static: false}) sort: MatSort;
   @ViewChild(MatTable, {static: false}) table: MatTable<TableItem>;
   dataSource: TableDataSource;
-
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  // displayedColumns = ['id', 'name', 'status', 'active', 'description', 'ip_address', 'is_shared', 'online'];
+  displayedColumns = ['id', 'name', 'minus7', 'minus6', 'minus5', 'minus4', 'minus3', 'minus2', 'minus1', 'now'];
+  nowminus7: any;
+  nowminus6: any;
+  nowminus5: any;
+  nowminus4: any;
+  nowminus3: any;
+  nowminus2: any;
+  nowminus1: any;
+  now: any;
 
   ngOnInit() {
     this.dataSource = new TableDataSource();
+    this.getDates();
   }
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
     this.table.dataSource = this.dataSource;
+  }
+
+getDates() {
+    const now = new Date();
+    this.nowminus7 = format(subDays(now, 7), 'ddMMM');
+    this.nowminus6 = format(subDays(now, 6), 'ddMMM');
+    this.nowminus5 = format(subDays(now, 5), 'ddMMM');
+    this.nowminus4 = format(subDays(now, 4), 'ddMMM');
+    this.nowminus3 = format(subDays(now, 3), 'ddMMM');
+    this.nowminus2 = format(subDays(now, 2), 'ddMMM');
+    this.nowminus1 = format(subDays(now, 1), 'ddMMM');
+    this.now = format(now, 'ddMMM');
+    console.log('now: ', this.now);
+    console.log('nowminus1: ', this.nowminus1);
+    console.log('nowminus2: ', this.nowminus2);
+    console.log('nowminus3: ', this.nowminus3);
+    console.log('nowminus4: ', this.nowminus4);
+    console.log('nowminus5: ', this.nowminus5);
+    console.log('nowminus6: ', this.nowminus6);
+    console.log('nowminus7: ', this.nowminus7);
   }
 }
