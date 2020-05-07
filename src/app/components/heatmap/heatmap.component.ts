588789878d4e8b8c71173e273c474f18d4773474
Martin Ryan <martynmryan@gmail.com>
2020-05-08 00:04:20 +1000

    live heatmap data from insight server

--- src/app/components/heatmap/heatmap.component.ts
+++ src/app/components/heatmap/heatmap.component.ts
@@ -8,7 +8,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { NotificationService } from './../../services/notification/notification.service';
 import { SettingsService } from './../../services/settings/settings.service';
 import { Memoize } from 'lodash-decorators/memoize';
-import {InsightService} from './../../services/insight-api/insight.service'
+import { InsightService } from './../../services/insight-api/insight.service'
 import { Runners } from 'src/app/models/runners.model';
 import { Runner } from './../../models/runner';
 import { each, map, reverse, uniq } from 'lodash';
@@ -19,51 +19,60 @@ import { each, map, reverse, uniq } from 'lodash';
   styleUrls: ['./heatmap.component.styl']
 })
 export class HeatmapComponent implements OnInit {
-  // yAxis = {
-  //   labels: ['shared-runner-1', 'shared-runner-2', 'shared-runner-3', 'shared-runner-4', 'shared-runner-5', 'shared-runner-6',
-  //   'shared-runner-7', 'runner-1', 'runner-2', 'runner-3', 'runner-4', 'runner-5', 'runner-6'],
-  // };
 
-  xAxis = {
-  //   labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'],
-  };
+  xAxis: Object = {};
+  yAxis: Object = {};
 
-titleSettings = {
+  heatmapData = [];
+
+  titleSettings = {
     text: '',
     textStyle: {
-        size: '15px',
-        fontWeight: '500',
-        fontStyle: 'Normal'
-}
-};
+      size: '15px',
+      fontWeight: '500',
+      fontStyle: 'Normal'
+    }
+  };
 
-legendSettings = {
+  legendSettings = {
     visible: true,
     position: 'Right',
     showLabel: true,
     height: '150px'
-};
+  };
 
-cellSettings = {
-  showLabel: false,
-  format: '{value}'
-};
+  paletteSettings = {
+    palette: [
+      { value: 0, color: '#FF3939' },
+      { value: 50, color: '#E85C27' },
+      { value: 100, color: '#60FF73' }
+    ]
+  };
 
-paletteSettings = {
-  palette: [
-  { value: 0, color: '#FF3939' },
-  { value: 50, color: '#E85C27' },
-  { value: 100, color: '#60FF73' }
-  ]
-};
+  dataSourceSettings: Object = {
+    isJsonData: true,
+    adaptorType: 'Cell',
+    xDataMapping: 'date',
+    yDataMapping: 'runner',
+    valueMapping: 'uptime'
+  };
 
-public isLoading = false;
-public timeRangeRunners: number;
-private subscriptions: Array<any> = [];
-public updateInterval = 60000;
-public uptimesLive: Array<Runner> = [];
-public rlabels: Array<String> = [];
-yAxis = {}
+  cellSettings: Object = {
+    border: {
+      radius: 4,
+      width: 1,
+      color: 'white'
+    },
+    showLabel: false,
+    format: '{value}',
+  };
+
+  public isLoading = false;
+  public timeRangeRunners: number;
+  private subscriptions: Array<any> = [];
+  public updateInterval = 60000;
+  public uptimesLive: Array<Runner> = [];
+  public rlabels: Array<String> = [];
 
 
   constructor(
@@ -73,19 +82,19 @@ yAxis = {}
     private spinner: NgxSpinnerService,
     private zone: NgZone,
     private iconReg: SvgIconRegistryService
-    ) { }
+  ) { }
 
   ngOnInit() {
     // this.timeRangeRunners = Number(this.settingsService.settings.timeRangeRunners);
     this.timeRangeRunners = 30 // for testing
     console.log('ngOnInit uptimes range: ', this.timeRangeRunners, ' days');
     this.fetchUptimesRangeDays(this.timeRangeRunners);
-      this.zone.runOutsideAngular(() => {
-        setInterval(() => {
-          this.clearSubscriptions();
-          this.fetchUptimesRangeDays(this.timeRangeRunners);
-        }, this.updateInterval);
-      });
+    this.zone.runOutsideAngular(() => {
+      setInterval(() => {
+        this.clearSubscriptions();
+        this.fetchUptimesRangeDays(this.timeRangeRunners);
+      }, this.updateInterval);
+    });
   }
 
   // @Memoize
@@ -95,52 +104,61 @@ yAxis = {}
     console.log('fetching uptimes range: ', ndays, ' days');
     console.log('');
     const daydataArray: Array<any> = [];
-    // const uptimesArray: Array<Runner> = [];
     const runners = []
     const uptimesArray = [];
     const labelsArray = [];
-    
-      this.api.fetchUptimes(ndays).subscribe(
-        uptimes => {
-          each(uptimes,  (value, key) => {
-            each(value,  (v, k) => {
-              if(k === 'timestamp') {
-                // console.log('k timestamp -> ', v)
-              }
-              if(k === 'runners') {
-                runners.push(v)
-              }
-              }
-            );
-          });
-          for (const r of runners) {
-            const dayd = [];
-            const labeld = [];
-            each(r,  (value, key) => {
-              // labeld.push(
-              //   {
-              //     runner: value[0],
-              //     uptime: value[1]
-              //   });
-              labeld.push(value[0]);
-              // dayd.push(value[0]);
-              dayd.push(value[1]);
-            })
-            uptimesArray.push(dayd);
-            labelsArray.push(labeld);
+    const celldata = []
+
+    this.api.fetchUptimes(ndays).subscribe(
+      uptimes => {
+        let timestring;
+        let datestring;
+        each(uptimes, (value, key) => {
+          console.log('');
+          each(value, (v, k) => {
+            if (k === '_id') {
+              const idobj = Object(v);
+              const idtstring = idobj.$date;
+              datestring = this.timestampToDate(idtstring);
+            }
+            if (k === 'runners') {
+              runners.push(v)
+            }
           }
-          this.uptimesLive = uptimesArray;
-          this.yAxis = {labels: reverse(uniq(labelsArray[0]))};
-          // console.log('uptimes -> uptimesLive ', this.uptimesLive);
-        },
-        err => {
-          this.isLoading = false;
-          this.spinner.hide();
-          this.notificationService.activeNotification.next({
-            message: err.message
-          });
+          );
+          for (const r of runners) {
+          const dayd = {};
+          const labeld = [];
+          each(r, (value, key) => {
+            const d =
+            {
+              'runner': value[0],
+              'uptime': String(value[1]),
+              'date': datestring
+            }
+            celldata.push(d);
+          })
+          console.log('cell data -> \n', celldata);
         }
-      );
+        this.heatmapData = celldata;
+        });
+      },
+      err => {
+        this.isLoading = false;
+        this.spinner.hide();
+        this.notificationService.activeNotification.next({
+          message: err.message
+        });
+      }
+    );
+  }
+
+  private timestampToDate(timestamp) {
+    const d = new Date(timestamp),
+      mm = ('0' + (d.getMonth() + 1)).slice(-2),
+      dd = ('0' + d.getDate()).slice(-2)
+    const day = dd + '-' + mm;
+    return day;
   }
 
   private clearSubscriptions() {
@@ -148,5 +166,4 @@ yAxis = {}
       sub.unsubscribe();
     });
   }
-
 }
